3a23640892b89b896cbf8ff8a882ded6b50c4b07
ChocolateThunder42 <malesela@bbd.co.za>
2022-03-08 13:09:52 +0200

    Classes and structs

---
+++ 03-classes-structs/structs/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Experiment
+{
+  /* *****************
+   * 
+   * Structs
+   * 
+   * Structs share most of the same syntax as classes.
+   * Structs are defined by using the struct keyword
+   * 
+   * *****************/
+
+    public struct PostalAddress
+    {
+      public int streetNumber;
+      public string addressLine1;
+      public string addressLine2;
+      public string city;
+      public string postalCode;
+
+      public void echoAddress()
+      {
+        string address = streetNumber + " " + addressLine1 + ", " + addressLine2 + ", " + city + ", " + postalCode;
+
+        Console.WriteLine("Address is: " + address);  
+      }
+    }
+
+    class Program
+    {
+      static void Main(String[] args)
+      {
+        PostalAddress address;
+
+        address.streetNumber = 1;
+        address.addressLine1 = "Newtown Ave";
+        address.addressLine2 = "Killarney";
+        address.city = "Johannesburg";
+        address.postalCode = "2193";
+
+        address.echoAddress();  
+      }
+    }
+
+}
