9919235338c812b1c2fbb8627cdc92c94a3e8352
ChocolateThunder42 <malesela@bbd.co.za>
2022-03-16 15:41:43 +0200

    Interfaces and their properties

--- 04-Object-Orientation/Program.cs
+++ 04-Object-Orientation/Program.cs
@@ -32,7 +32,16 @@
  *     such as method parameters and collections or arrays.
  *
  * ii) Base classes may define and implement virtual methods, 
- *     and derived classes can override them.
+ *     and derived classes can override them. They provide 
+ *     their own definition and implementation.
+ *     
+ * The difference between Virtual methods and Abstract methods
+ * is that:
+ * 
+ * Virtual methods have an implementation and provide the derived 
+ * classes with the option of overriding it. While abstract methods 
+ * do not provide an implementation and force the derived classes to 
+ * override the method.
  * 
  * *****************/
 
@@ -56,6 +65,7 @@ namespace Experiment
   public abstract class Animal
   {
     public string? Name { get; set; }
+    private const int _age = 5;
 
     public abstract string MakesSound();
 
@@ -63,15 +73,26 @@ namespace Experiment
     {
       return $"{Name} moved {distance} meters";
     }
+
+    public virtual int GetAge()
+    {
+      return (_age); 
+    }
   }
 
   public class Dog : Animal
   {
     public bool HasTail { get; set; }
 
-    public override string MakesSound()
+    public override string MakesSound() => "Bark bark";
+
+    public override int GetAge()
     {
-        return "Bark bark";
+      // We are accessing the Animal GetAge method
+      // by using the keyword base and then overriding
+      // what the method returns.
+
+      return (base.GetAge() * 7);    
     }
   }
 
@@ -94,20 +115,74 @@ namespace Experiment
     public string? Colour { get; set; }  
   }
 
+  /* *************************
+   * 
+   * Interfaces
+   * 
+   * An interface contains definitions for a group 
+   * of related functionalities that a class or a 
+   * struct can implement. It has the following properties:
+   * 
+   * + Any class/struct that implements the 
+   *   interface must implement all its members.
+   *   
+   * + An interface can't be instantiated directly.
+   *   Its members are implemented by any class 
+   *   that implements the interface.
+   *   
+   * + They can contain events, indexers, methods, and 
+   *   properties.
+   *   
+   * + They contain no implementation of methods.
+   * 
+   * + A class or struct can implement multiple interfaces.
+   *   Along with inheriting a base class.
+   *   
+   * + There are two main differences between an interface 
+   *   and an abstract class:
+   *   
+   *   i)  An interface can not contain method 
+   *       implementations, an abstract class can.
+   *       
+   *   ii) A class can implement multiple interfaces, 
+   *       but it can only inherit from one class
+   * 
+   * *************************/
+
+   public interface IAnimal
+   {
+     string MakesSound();
+     string Move();
+     int GetAge(); 
+   }
+
+   public class CoolAnimal : IAnimal
+   {
+     public string MakesSound() => "I'm an oger";
+
+     public string Move() => "You're moving";
+
+     public int GetAge() => 300;
+   }
+
   class Program
   {
     static void Main(string[] args)
     {
       Dog myDog = new Dog();
       Bullterrier myOtherDog = new Bullterrier();
+      CoolAnimal coolAnimal = new CoolAnimal();
 
-      myDog.Name = "Max"
+      myDog.Name = "Max";
       myOtherDog.Name = "Hellhound";
       myOtherDog.Colour = "red";
       
-      Console.log("Over here...");
+      Console.WriteLine("Over here...");
       Console.WriteLine(myDog.Move(9));
       Console.WriteLine(myOtherDog.Move(14));
+
+      Console.WriteLine(coolAnimal.MakesSound());
+      Console.WriteLine(coolAnimal.Move());
 
       Console.WriteLine("\nPress any key to exit.");
       Console.ReadKey();
